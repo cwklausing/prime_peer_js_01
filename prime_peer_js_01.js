69903e8ede0082ce07f223e8e362897b386222f2
cwklausing <cwklausing@gmail.com>
2015-11-11 15:19:27 -0600

    Update prime_peer_js_01.js

--- prime_peer_js_01.js
+++ prime_peer_js_01.js
@@ -38,8 +38,43 @@ console.log(prime)
 value = value.join('');
 prime = prime.join('');
 
+//Step 7
 value = parseInt(value);
 prime = parseInt(prime);
 
-console.log(value)
-console.log(prime)
+console.log(value);
+console.log(prime);
+
+//Step 8
+value += prime;
+
+//Step 9
+if (value < 60) {
+  value = 14;
+} else if (value === 2930){
+  value = 27;
+} else { 
+  value = 2;
+}
+
+//Step 10
+var i = 10;
+while (i > 0) {
+  value++;
+  i--;
+}
+//Step 11
+function makeString (val) {
+  val = val.toString();
+  if (val.length > 1) {
+    val.shift();
+  }
+  value = val;
+  return val;
+};
+
+//step 12
+makeString(value);
+
+//step 13
+console.log(value);
